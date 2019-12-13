95918154b2ec36c1e66ba89339a0141cf8cc48c1
Ehaschia <guiwuqiansha@gmail.com>
2019-12-13 16:59:36 +0900

    runner update

--- tunner/runner.py
+++ tunner/runner.py
@@ -17,9 +17,9 @@ def runner(idx, cli):
     stdin, stdout, stderr = client.exec_command(cli_prefix + cli)
     exit_status = stdout.channel.recv_exit_status()  # Blocking call
     if exit_status == 0:
-        print('Task ' + cli + ' Done')
+        print('Task ' + cli + ' Done at Thread ' + str(idx))
     else:
-        print('Error ' + str(exit_status) + 'of ' + cli)
+        print('Error ' + str(exit_status) + 'of ' + cli + ' at Thread ' + str(idx))
     client.close()
     return idx, cli
 
@@ -27,23 +27,29 @@ def runner(idx, cli):
 def multiprocess(configs, thread):
     thread = min(len(configs), thread)
     pool = Pool(processes=thread)
-    runnings = []
+    runnings = {}
     finished = []
+    # init threads
     for i in range(1, thread + 1):
         time.sleep(0.1)
-        runnings.append(pool.apply_async(runner, (i, configs.pop())))
+        runnings[i] = pool.apply_async(runner, (i, configs.pop()))
+
     while len(finished) != len(configs):
         time.sleep(10)
-        for res in runnings:
-            if res.ready():
-                idx, cli = res.get()
-                finished.append(cli)
-                print(str(idx) + ' ' + cli + ' Finished')
-                runnings.remove(res)
-                if len(configs) > 0:
-                    runnings.append(pool.apply_async(runner, (idx, configs.pop())))
+        activate_thread = 0
+        for idx in runnings.keys():
+            if runnings[idx] is not None:
+                activate_thread += 1
+                if runnings[idx].ready():
+                    finished_thread = runnings[idx]
+                    runnings[idx] = None
+                    idx, cli = finished_thread.get()
+                    finished.append(cli)
+                    if len(configs) > 0:
+                        runnings[idx] = pool.apply_async(runner, (idx, configs.pop()))
             else:
                 pass
+        print("Activate thread: " + str(activate_thread))
 
 
 if __name__ == '__main__':
